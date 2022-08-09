b1f87f27f8bfd009df9891b308b0a0e36e54ef6c
Rayane Nossair <rnossairx@gmail.com>
2022-08-09 18:10:22 +0200

    Prepared basic UI for password submission

--- react-backend/client/src/App/pages/passList.js
+++ react-backend/client/src/App/pages/passList.js
@@ -4,12 +4,34 @@ class passList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            passwords: []
+            passwords: [],
+            nameInput: "",
+            passInput: ""
         }
+        this.handleNameInput = this.handleNameInput.bind(this);
+        this.handlePassInput = this.handlePassInput.bind(this);
+    }
+    handleNameInput(e){
+        this.setState({lengthInput: e.target.value})
+    }
+    handlePassInput(e){
+        this.setState({lengthInput: e.target.value})
+    }
+    generatePassword(){
+
     }
     render(){
-        return(<div class="passListPage">
+        return(<div className="passListPage">
             <h1>List:</h1>
+            <div id="registerPass">
+                <h4>Add new password:</h4>
+                <div className="inputContainer">
+                    <input type="text" placeholder="Website/Service" onChange={this.handleNameInput}></input>
+                    <input type="text" placeholder="Password" onChange={this.handleNameInput}></input>
+                    <button onClick={this.generatePassword}>Generate Secure Password</button>
+                    <button>Submit</button>
+                </div>
+            </div>
         </div>
         )
     }
