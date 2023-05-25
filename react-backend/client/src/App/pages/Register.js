a471a02744f77710ed44d38297746a9b1a0bfc67
Rayane Nossair <rnossairx@gmail.com>
2023-05-25 16:56:23 +0100

    cre

--- react-backend/client/src/App/pages/Register.js
+++ react-backend/client/src/App/pages/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
     register() {
         fetch("https://password-manager-server.vercel.app/api/register", {
-            method: "POST",credentials: 'include' headers: {
+            method: "POST",credentials: 'include', headers: {
                 'Content-Type': 'application/json',
             }, body: JSON.stringify({ username: this.state.username, password: this.state.password })
         })
